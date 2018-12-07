810a0b4df84020ffc558fa88d6c5abb261a10303
xwrexx <wrex1285@gmail.com>
2018-12-07 12:51:51 -0700

    changed class to className in login.js

--- src/components/login.js
+++ src/components/login.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Login = (props) => {
     return(
-        <div class="container">
+        <div className="container">
   
-            <div class="row" id="pwd-container">
-                <div class="col-md-4"></div>
+            <div className="row" id="pwd-container">
+                <div className="col-md-4"></div>
                 
-                <div class="col-md-4">
-                <section class="login-form">
+                <div className="col-md-4">
+                <section className="login-form">
                     <form method="post" action="#" role="login">
-                    <img src="http://i.imgur.com/RcmcLv4.png" class="img-responsive" alt="" />
-                    <input type="email" name="email" placeholder="Email" required class="form-control input-lg" value="joestudent@gmail.com" />
-                    <input type="password" class="form-control input-lg" id="password" placeholder="Password" required="" />
-                    <div class="pwstrength_viewport_progress"></div>
-                    <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Sign in</button>
+                    <img src="http://i.imgur.com/RcmcLv4.png" className="img-responsive" alt="" />
+                    <input type="email" name="email" placeholder="Email" required className="form-control input-lg" value="joestudent@gmail.com" />
+                    <input type="password" className="form-control input-lg" id="password" placeholder="Password" required="" />
+                    <div className="pwstrength_viewport_progress"></div>
+                    <button type="submit" name="go" className="btn btn-lg btn-primary btn-block">Sign in</button>
                     <div>
                         <a href="#">Create account</a> or <a href="#">reset password</a>
                     </div>
